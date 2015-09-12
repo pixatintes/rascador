8dd37ce5f5e3e01c8f0536768a754b2177c156a1
pixatintes <pixatintes@yahoo.es>
2015-09-13 01:30:42 +0200

    Update grata.sh

--- gratador/grata.sh
+++ gratador/grata.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Answer=\'`zenity --entry --title="Grata laBsk" --text="Paraula clau a buscar:" --entry-text "gmt"`\'
+Answer=`zenity --entry --title="Grata laBsk" --text="Paraula clau a buscar:" --entry-text "gmt"`
 
 VALUE=`zenity --scale --text="Pàgines a buscar:" --value="1" --min-value="1" --max-value="25" –step="1"`
 
